3deaf3052388c344413a188b85326d1188c7b46a
Muzammil Rafay <36181738+MuzammilRafay@users.noreply.github.com>
2026-06-14 21:57:37 +0500

    learn event loop

---
+++ Sync & Async & Event Loop/app.js
@@ -0,0 +1,26 @@
+// Synchronous
+// Code runs line by line
+
+// console.log("html/css");
+// console.log("js");
+// console.log("react js");
+// console.log("react native");
+
+//Asynchronous programming
+//we have to wait some thing in code
+console.log("html/css");
+setTimeout(() => {
+  console.log("js");
+}, 0);
+// }, 1000);
+console.log("react js");
+console.log("react native");
+
+//is js is asyncronous by default ?
+//answer is no
+
+//thread = how much task that will handle in js
+
+//javascript is single thread
+
+//thread means one worker at a time
